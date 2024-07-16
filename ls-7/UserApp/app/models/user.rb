1161150562c51a9a14bbdf9ad48310cb2778faa5
User <vladshimon2005@gmail.com>
2024-07-16 05:41:56 -0700

    Add db modesl vith validation

--- ls-7/UserApp/app/models/user.rb
+++ ls-7/UserApp/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+  has_many :LabReport
   validate :FirstName, presense: true, length:{ maximum: 100}
   validate :LastName, presense: true, length:{ maximum: 100}
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
